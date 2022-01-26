a8cb882735210059cc46c9d0b29a87ee533f9573
sokolserg <sergsokolkin@gmail.com>
2022-01-26 09:06:55 +0300

    next

--- avr/admin.py
+++ avr/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AVRLog1, AVRLog2, AVRLog3
 
-# Register your models here.
+admin.site.register(AVRLog1)
+admin.site.register(AVRLog2)
+admin.site.register(AVRLog3)
